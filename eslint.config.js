1eec5b5f9f6d95dcdc76742d63f3ac92a6f60aaa
hermanpe <hermanpe@stud.ntnu.no>
2025-03-28 09:41:07 +0100

    start of rewrite to nextjs

---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import typescriptParser from '@typescript-eslint/parser'
+import stylisticJs from '@stylistic/eslint-plugin-js'
+
+export default [
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        plugins: {
+            '@stylistic/js': stylisticJs,
+        },
+        languageOptions: {
+            sourceType: 'module',
+            ecmaVersion: 2024,
+            parser: typescriptParser
+        },
+        rules: {
+            'strict': 'error',
+            'no-var': 'error',
+            'array-callback-return': 'error',
+            'yoda': 'error',
+            '@stylistic/js/indent': [
+                'error',
+                4,
+            ],
+            '@stylistic/js/quotes': [
+                'error',
+                'single'
+            ],
+            '@stylistic/js/semi': [
+                'error',
+                'never'
+            ],
+            '@typescript-eslint/no-unused-vars': 'error',
+            '@typescript-eslint/ban-ts-comment': 'off',
+            '@typescript-eslint/no-non-null-assertion': 'off'
+        }
+    }
+];
