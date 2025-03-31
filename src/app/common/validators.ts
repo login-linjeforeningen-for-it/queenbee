558f84ab33c2df19e7b3719c328f25047427d1d4
hermanpe <hermanpe@stud.ntnu.no>
2025-03-31 11:46:43 +0200

    delete angular files

--- src/app/common/validators.ts
+++
@@ -1,9 +0,0 @@
-import { FormControl, ValidationErrors } from "@angular/forms"
-
-export function NoDecimalValidator(): ValidationErrors | null {
-    return (control: FormControl) => {
-        const isDecimal = Number(control.value) % 1 != 0
-        const error = isDecimal ? { 'decimal': true } : null
-        return error
-    }
-}
--- src/app/pages/jobads/jobads.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-jobads',
-  templateUrl: './jobads.component.html'
-})
-export class JobadsComponent {
-}
