2c4cbfd253bf193f61d3b16cfa3b117362653bdb
gjermundhp02 <gjermund.pedersen@gmail.com>
2025-04-05 14:18:09 +0200

    Started modal

--- src/app/rules/page.tsx
+++ src/app/rules/page.tsx
@@ -1,7 +1,18 @@
+"use client"
+import Modal from "@components/modal/modal";
+import { useState } from "react";
+
 export default function page() {
+    const [showModal, setShowModal] = useState(false);
+
     return (
-        <div>
-            <h1 className="font-semibold text-lg">Rules</h1>
+        <div className="w-full">
+            <button onClick={()=>{setShowModal(true)}}>New rule</button>
+            {showModal && (
+                Modal({
+                    children: <div>Hello</div>,
+                    onClose: () => {setShowModal(false)}
+                }))}
         </div>
     )
 }
