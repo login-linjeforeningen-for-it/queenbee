497080409671423694784ec8bdb326f6c7c2dc16
hermanpe <hermanpe@stud.ntnu.no>
2025-04-05 18:14:08 +0200

    navbar

--- src/components/navbar/nav.tsx
+++ src/components/navbar/nav.tsx
@@ -1,10 +1,13 @@
-import Image from "next/image";
-import ThemeSwitch from "../theme/themeSwitch";
+import Image from 'next/image'
+import ThemeSwitch from '../theme/themeSwitch'
+import Link from 'next/link'
+import Login from '@components/svg/login'
+import Logout from '@components/svg/logout'
 
 
 export default function Nav() {
     return (
-        <nav className="relative w-full bg-black h-[45px] flex justify-between">
+        <nav className='relative w-full bg-black h-[45px] flex justify-between'>
             <LeftSide />
             <RightSide />
         </nav>
@@ -13,19 +16,29 @@ export default function Nav() {
 
 function LeftSide() {
     return (
-        <div className="flex gap-4">
-            <div className="relative h-[45px] w-[45px]">
-                <Image alt="Logo" src="/images/queenbee-logo.png" fill={true} quality={100} />
+        <div className='flex gap-4'>
+            <div className='relative h-[45px] w-[45px]'>
+                <Link
+                    href={'/'}    
+                >
+                    <Image alt='Logo' src='/images/queenbee-logo.png' fill={true} quality={100} />
+                </Link>
             </div>
-            <h1 className="self-center font-semibold text-white">QUEENBEE - Admintool</h1>
+            <h1 className='self-center font-semibold text-white'>QUEENBEE - Admintool</h1>
         </div>
     )
 }
 
 function RightSide() {
+    const loggedIn = false 
+
     return (
-        <div>
+        <div className='flex gap-[1rem] items-center pr-[1rem]'>
             <ThemeSwitch />
+            <Link className='flex gap-[0.5rem] hover:*:text-[var(--color-text-primary)] hover:*:fill-[var(--color-text-primary)]' href={loggedIn ? '/logout' : '/login'}>
+                {loggedIn ? <h1>Logg ut</h1> : <h1>Logg inn</h1>}
+                {loggedIn ? <Logout className='fill-white w-[1.5rem]' /> : <Login className='fill-white w-[1.5rem]'/>}
+            </Link>
         </div>
     )
 }
---
+++ src/components/svg/logout.tsx
@@ -0,0 +1,7 @@
+export default function Logout({className}:{className:string}) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className={className}>
+            <g><path d="M0,0h24v24H0V0z" fill="none"/></g><g><g><polygon points="5,5 12,5 12,3 3,3 3,21 12,21 12,19 5,19"/><polygon points="21,12 17,8 17,11 9,11 9,13 17,13 17,16"/></g></g>
+        </svg>
+    )
+}
