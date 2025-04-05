a26f30fb2e533465ad1f698514cfdb3a2408768f
gjermundhp02 <gjermund.pedersen@gmail.com>
2025-04-05 14:40:56 +0200

    Created modal

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,10 +1,15 @@
 
-
-export default function Modal({ children, onClose }: { children: React.ReactNode; onClose: () => void }) {
+export default function Modal({ children, close }: { children: React.ReactNode; close: () => void }) {
    return (
-      <div style={{position: "absolute", top:0, left:0, right:0, bottom:0, backgroundColor: "rgba(0, 0, 0, 0.5)", zIndex: 100}}>
-         <div style={{justifySelf: "center", alignSelf: "center", backgroundColor: "white", width: "50%", height: "50%", margin: "auto", padding: 20, borderRadius: 10}}></div>
-
+      <div className="absolute w-full h-full justify-center flex"
+         onClick={(e)=>{
+            if(e.currentTarget==e.target){
+               close()
+            }
+         }}>
+         <div className="bg-background">
+            {children}
+         </div>
       </div>
    )
 }
