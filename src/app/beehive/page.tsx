607e9178dcb96cd72564c27b16b908387cd8799c
eirikhanasand <eirik.hanasand@gmail.com>
2025-04-06 15:03:26 +0200

    Added note

--- src/app/beehive/page.tsx
+++ src/app/beehive/page.tsx
@@ -2,6 +2,9 @@ export default function page() {
     return (
         <div className='h-[var(--h-pageInfo)]'>
             <h1 className='font-semibold text-lg'>Beehive (til management av tekst på beehive)</h1>
+            <div className="grid place-items-center self-center h-full mb-2">
+                <h1>Denne funksjonaliteten mangler ett nytt API som er integrert med både Beehive, Queenbee og Nucleus.</h1>
+            </div>
         </div>
     )
 }
