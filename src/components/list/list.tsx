31c75198e9f23c57acacb84a51366f9e52ddcb96
olemgl <olemgl@stud.ntnu.no>
2025-04-06 20:34:55 +0200

    removed length check

--- src/components/list/list.tsx
+++ src/components/list/list.tsx
@@ -20,14 +20,15 @@ type BodyProps = {
 
 export default function List({list, sticky, visible}: ListProps) {
     const keys = Object.keys(list[0])
-
+        {/*return should wrap if the elements dont have too many elements on hover*/}
     return (
-        <div className='relative overflow-visible overflow-y-auto max-h-[calc(100vh-var(--h-navbar)-var(--h-pageInfo))] noscroll w-fullo max-p-100 max-w-[calc(100vw-var(--w-sidebar))]'>
+        <div className='relative overflow-y-auto max-h-[calc(100vh-var(--h-navbar)-var(--h-pageInfo))] min-h-[calc(100vh-var(--h-navbar)-var(--h-pageInfo))] noscroll w-fullo max-p-100 max-w-[calc(100vw-var(--w-sidebar))]'>
             <table className='w-full border-separate border-spacing-[1px] table-auto rounded-lg overflow-hidden'>
                 <Header keys={keys} sticky={sticky} visible={visible} />
                 <Body list={list} sticky={sticky} visible={visible} />
             </table>
         </div>
+
     )
 }
 
@@ -56,8 +57,7 @@ function Body({list, sticky, visible}: BodyProps) {
     return list.map((_, index) => {
         const entries = Object.entries(list[index])
         const maxChars = 18
-        let end = false
-        if(list.length > 8) end = index >= list.length - 4
+        const end = index >= list.length - 3
         return (
             <tbody key={index} className='bg-extralight'>
                 <tr>
@@ -73,7 +73,7 @@ function Body({list, sticky, visible}: BodyProps) {
                                         {String(value)}
                                     </h1>
                                     {String(value).length > maxChars && (
-                                        <div className={`absolute left-0 z-[1000] hidden group-hover:block bg-normal p-2 rounded max-w-xs break-words whitespace-normal border border-foreground ${(end) ? "bottom-full" : ""}`}>
+                                        <div className={`absolute left-0 z-[1000] hidden group-hover:block bg-normal p-2 rounded max-w-xs break-words whitespace-normal border border-foreground ${(end) ? 'bottom-full' : ""}`}>
                                             {String(value)}
                                         </div>
                                     )}
