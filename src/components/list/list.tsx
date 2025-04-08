0768aa16655897b1b1fa3d6ac40367efc9d1e97b
hermanpe <hermanpe@stud.ntnu.no>
2025-04-08 16:00:50 +0200

    sort arrows

--- src/components/list/list.tsx
+++ src/components/list/list.tsx
@@ -1,3 +1,10 @@
+'use client'
+
+import ArrowDown from '@components/svg/arrowDown'
+import ArrowUp from '@components/svg/arrowUp'
+import Link from 'next/link'
+import { useState } from 'react'
+
 type ListProps = {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     list: any[]
@@ -20,6 +27,7 @@ type BodyProps = {
 
 export default function List({list, sticky, visible}: ListProps) {
     const keys = Object.keys(list[0])
+
     return (
         <div className='relative h-fit w-full max-h-[calc(((100vh-var(--h-navbar))-var(--h-pageInfo))-2rem)] noscroll overflow-scroll'>
             <table className='relative h-full w-full border-separate border-spacing-[1px] rounded-lg'>
@@ -32,6 +40,20 @@ export default function List({list, sticky, visible}: ListProps) {
 }
 
 function Header({keys, sticky, visible}: HeaderProps) {
+    const [column, setColumn] = useState(keys[0])
+    const [sort, setSort] = useState<'asc'|'desc'>('asc')
+
+    function handleChange(key:string){
+        if(key === column) {
+            setSort(sort === 'asc' ? 'desc' : 'asc')
+        } else {
+            setColumn(key)
+            setSort('asc')
+        }
+        
+        
+    }
+
     return (
         <thead className='bg-extralight h-[2rem]'>
             <tr>
@@ -42,8 +64,23 @@ function Header({keys, sticky, visible}: HeaderProps) {
                     }
 
                     return (
-                        <th key={key} className={`max-w-[10rem] p-2 text-left ${sticky.includes(key) ? 'shadow-[1px_0_0_0_var(--color-dark)] font-bold sticky left-0 z-10 bg-extralight' : 'font-normal'}`}>
-                            {value}
+                        <th key={key} className={`whitespace-nowrap text-left ${sticky.includes(key) ? 'shadow-[1px_0_0_0_var(--color-dark)] font-bold sticky left-0 z-10 bg-extralight' : 'font-normal'}`}>
+                            <Link
+                                href=''
+                                className='w-full h-full p-[0.5rem] flex flex-row items-center justify-between group'
+                                onClick={() => handleChange(key)}
+                            >
+                                <h1>{value}</h1>
+                                {column === key ? (
+                                    sort === 'asc' ? (
+                                        <ArrowUp className='h-[1.5rem] fill-bright' />
+                                    ) : (
+                                        <ArrowDown className='h-[1.5rem] fill-bright' />
+                                    )
+                                ) : <ArrowUp className='h-[1.5rem] fill-almostbright opacity-0 group-hover:opacity-100' />
+                                }
+                                
+                            </Link>
                         </th>
                     )
                 })}
@@ -72,7 +109,7 @@ function Body({list, sticky, visible}: BodyProps) {
                                         {String(value)}
                                     </h1>
                                     {String(value).length > maxChars && (
-                                        <div className={`absolute left-0 z-999 hidden group-hover:block bg-normal p-2 rounded max-w-xs break-words whitespace-normal border border-foreground ${(end) ? 'bottom-full' : ""}`}>
+                                        <div className={`absolute left-0 z-999 hidden group-hover:block bg-normal p-2 rounded max-w-xs break-words whitespace-normal border border-foreground ${(end) ? 'bottom-full' : ''}`}>
                                             {String(value)}
                                         </div>
                                     )}
