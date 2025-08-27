7c834fba37e52b6f41471872f27595122255fa63
eirikhanasand <eirik.hanasand@gmail.com>
2025-08-27 05:49:35 +0200

    fixed quotes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,8 @@
-import type { Config } from "tailwindcss"
+import type { Config } from 'tailwindcss'
 
 export default {
     content: [
-        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}'
     ]
 } satisfies Config
