e58c3f16f34b968ecb6b12bceb5a427a3b15dcdb
eirikhanasand <eirik.hanasand@gmail.com>
2025-09-17 17:58:34 +0200

    Fixed bugs

--- src/components/dashboard/totalStats.tsx
+++ src/components/dashboard/totalStats.tsx
@@ -7,51 +7,38 @@ import {
     Building2,
     MapPin
 } from 'lucide-react'
-import { useEffect, useState } from 'react'
+import { ReactNode, useEffect, useState } from 'react'
 
 export default function TotalStats({ stats }: { stats: DashboardTotalStats }) {
     return (
         <div className='mt-4 grid grid-cols-2 md:grid-cols-5 gap-4'>
-            <div className='p-4 bg-white/5 rounded-md flex flex-col justify-center h-24'>
-                <div className='flex items-center gap-2 text-sm text-muted-foreground'>
-                    <Calendar className='w-5 h-5' /> Events
-                </div>
-                <div className='font-bold text-2xl'><CountUp end={stats.events} /></div>
+            <Stat text='Events' count={stats.events} item={<Calendar className='w-5 h-5' />} />
+            <Stat text='Jobs' count={stats.jobs} item={<BriefcaseBusiness className='w-5 h-5' />} />
+            <Stat text='Announcements' count={stats.announcements} item={<Megaphone className='w-5 h-5' />} />
+            <Stat text='Organizations' count={stats.organizations} item={<Building2 className='w-5 h-5' />} />
+            <Stat text='Locations' count={stats.locations} item={<MapPin className='w-5 h-5' />} />
+        </div>
+    )
+}
+
+function Stat({ item, count, text }: { item: ReactNode, count: number, text: string }) {
+    return (
+        <div className='p-4 bg-white/5 rounded-md flex flex-col justify-center h-24'>
+            <div className='flex items-center gap-2 text-sm text-muted-foreground'>
+                {item} {text}
             </div>
-            <div className='p-4 bg-white/5 rounded-md flex flex-col justify-center h-24'>
-                <div className='flex items-center gap-2 text-sm text-muted-foreground'>
-                    <BriefcaseBusiness className='w-5 h-5' /> Jobs
-                </div>
-                <div className='font-bold text-2xl'><CountUp end={stats.jobs} /></div>
-            </div>
-            <div className='p-4 bg-white/5 rounded-md flex flex-col justify-center h-24'>
-                <div className='flex items-center gap-2 text-sm text-muted-foreground'>
-                    <Megaphone className='w-5 h-5' /> Announcements
-                </div>
-                <div className='font-bold text-2xl'><CountUp end={stats.locations} /></div>
-            </div>
-            <div className='p-4 bg-white/5 rounded-md flex flex-col justify-center h-24'>
-                <div className='flex items-center gap-2 text-sm text-muted-foreground'>
-                    <Building2 className='w-5 h-5' /> Organizations
-                </div>
-                <div className='font-bold text-2xl'><CountUp end={stats.organizations} /></div>
-            </div>
-            <div className='p-4 bg-white/5 rounded-md flex flex-col justify-center h-24'>
-                <div className='flex items-center gap-2 text-sm text-muted-foreground'>
-                    <MapPin className='w-5 h-5' /> Locations
-                </div>
-                <div className='font-bold text-2xl'><CountUp end={stats.locations} /></div>
-            </div>
+            <div className='font-bold text-2xl'><CountUp end={count} /></div>
         </div>
     )
 }
 
 function CountUp({ end, duration = 1 }: { end: number, duration?: number }) {
     const [count, setCount] = useState(0)
+
     useEffect(() => {
         let start = 0
         const increment = end / (duration * 60)
-        const step = () => {
+        function step() {
             start += increment
             if (start < end) {
                 setCount(Math.floor(start))
@@ -62,5 +49,6 @@ function CountUp({ end, duration = 1 }: { end: number, duration?: number }) {
         }
         step()
     }, [end])
+
     return <span>{count}</span>
 }
