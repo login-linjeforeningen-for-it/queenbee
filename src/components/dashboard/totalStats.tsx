1ee35c049ab8e0f052146b1faf21984ade1ea7bd
0herm <0herm@protonmail.com>
2025-11-13 13:52:03 +0100

    link totalstats

--- src/components/dashboard/totalStats.tsx
+++ src/components/dashboard/totalStats.tsx
@@ -8,29 +8,30 @@ import {
     MapPin,
     Images
 } from 'lucide-react'
+import Link from 'next/link'
 import { ReactNode, useEffect, useState } from 'react'
 
 export default function TotalStats({ stats }: { stats: DashboardTotalStats }) {
     return (
         <div className='mt-4 grid grid-cols-2 md:grid-cols-6 gap-4'>
-            <Stat text='Events' count={stats.events} item={<Calendar className='w-5 h-5' />} />
-            <Stat text='Jobs' count={stats.jobs} item={<BriefcaseBusiness className='w-5 h-5' />} />
-            <Stat text='Announcements' count={stats.announcements} item={<Megaphone className='w-5 h-5' />} />
-            <Stat text='Organizations' count={stats.organizations} item={<Building2 className='w-5 h-5' />} />
-            <Stat text='Locations' count={stats.locations} item={<MapPin className='w-5 h-5' />} />
-            <Stat text='Albums' count={stats.albums} item={<Images className='w-5 h-5' />} />
+            <Stat type='events' count={stats.events} item={<Calendar className='w-5 h-5' />} />
+            <Stat type='jobs' count={stats.jobs} item={<BriefcaseBusiness className='w-5 h-5' />} />
+            <Stat type='announcements' count={stats.announcements} item={<Megaphone className='w-5 h-5' />} />
+            <Stat type='organizations' count={stats.organizations} item={<Building2 className='w-5 h-5' />} />
+            <Stat type='locations' count={stats.locations} item={<MapPin className='w-5 h-5' />} />
+            <Stat type='albums' count={stats.albums} item={<Images className='w-5 h-5' />} />
         </div>
     )
 }
 
-function Stat({ item, count, text }: { item: ReactNode, count: number, text: string }) {
+function Stat({ type, count, item }: { type: string, count: number, item: ReactNode }) {
     return (
-        <div className='p-4 bg-white/5 rounded-md flex flex-col justify-center h-24'>
-            <div className='flex items-center gap-2 text-sm text-muted-foreground'>
-                {item} {text}
+        <Link href={`/dashboard/${type}`} className='p-4 bg-white/5 rounded-md flex flex-col justify-center h-24'>
+            <div className='flex items-center gap-2 text-sm text-muted-foreground capitalize'>
+                {item} {type}
             </div>
             <div className='font-bold text-2xl'><CountUp end={count} /></div>
-        </div>
+        </Link>
     )
 }
 
