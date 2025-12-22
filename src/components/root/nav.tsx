d184e55be8033808d864f550c2907e9f018b6b6e
eirikhanasand <eirik.hanasand@gmail.com>
2025-12-22 20:55:49 +0100

    Fixed canonical classes

--- src/components/root/nav.tsx
+++ src/components/root/nav.tsx
@@ -7,11 +7,11 @@ import ArrowOutward from '@/components/svg/arrowOutward'
 
 export default function Navbar() {
     return (
-        <div className='flex justify-between bg-black h-full w-full md:px-[1rem] gap-2 overflow-hidden'>
+        <div className='flex justify-between bg-black h-full w-full md:px-4 gap-2 overflow-hidden'>
             <Menu />
             <ToolTips />
             {/* logo */}
-            <div className='flex items-center h-[3rem] md:w-[10rem]'>
+            <div className='flex items-center h-12 md:w-40'>
                 <Link href='/' className='relative block h-8 w-8'>
                     <Image
                         src={'/images/logo/logo.svg'}
@@ -21,15 +21,15 @@ export default function Navbar() {
                     />
                 </Link>
             </div>
-            <nav className='flex justify-between items-center w-fill max-w-[40rem]'>
+            <nav className='flex justify-between items-center w-fill max-w-160'>
                 <Link href='https://login.no'>
                     <li className={`
                         text-white flex flex-row items-center list-none 
-                        no-underline leading-[1rem] p-3 pr-[1.5rem] font-medium 
+                        no-underline leading-4 p-3 pr-6 font-medium 
                         cursor-pointer link--corner-hover
                     `}>
                         Login
-                        <ArrowOutward className='absolute right-[0.25rem] w-[1rem] h-[1rem] fill-login'/>
+                        <ArrowOutward className='absolute right-1 w-4 h-4 fill-login'/>
                     </li>
                 </Link>
             </nav>
