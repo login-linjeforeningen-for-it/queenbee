83f941f405a16ea24a90312a9b204886b5023c8d
eirikhanasand <eirik.hanasand@gmail.com>
2025-12-24 03:25:52 +0100

    Removed constraint on form.note

--- src/components/status/editService.tsx
+++ src/components/status/editService.tsx
@@ -60,7 +60,7 @@ export default function EditService({
 
     function isValid() {
         if (!form.name || !form.type || (form.type === 'fetch' && !form.url) || !form.interval ||
-            form.maxConsecutiveFailures === null || !form.note
+            form.maxConsecutiveFailures === null
         ) {
             return false
         }
