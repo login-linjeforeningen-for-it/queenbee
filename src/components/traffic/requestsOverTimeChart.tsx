817730d6bda0b92120f4b7e6c002f09885c3398b
eirikhanasand <eirik.hanasand@gmail.com>
2025-12-26 23:30:48 +0100

    removed leading newline

--- src/components/traffic/requestsOverTimeChart.tsx
+++ src/components/traffic/requestsOverTimeChart.tsx
@@ -1,4 +1,3 @@
-
 export default function RequestsOverTimeChart({ data }: { data: { key: string, count: number }[] }) {
     if (!data || data.length === 0) {
         return <div className='p-4 text-sm text-muted-foreground'>No data available</div>
