342134d2ecb576b2d9c906fda4302ae743b1b492
eirikhanasand <eirik.hanasand@gmail.com>
2025-12-26 04:23:53 +0100

    fixed styling

--- src/components/internal/info.tsx
+++ src/components/internal/info.tsx
@@ -11,7 +11,7 @@ export default function InternalInfo({ information }: InternalInfoProps) {
 
     return (
         <div className='flex gap-4'>
-            <Link href='/sites' className='p-4 bg-login-50/5 rounded-md flex flex-col justify-center w-42.5'>
+            <Link href='/sites' className='p-4 bg-login-50/5 rounded-md w-42.5'>
                 <h1 className='flex items-center gap-2 text-sm text-muted-foreground capitalize pb-4'>
                     <Star className='w-5' /> Primary Site
                 </h1>
@@ -24,16 +24,25 @@ export default function InternalInfo({ information }: InternalInfoProps) {
                     <h1 className={secondary}>{information.primarySite.name}</h1>
                 </div>
             </Link>
-            <Link href='/sites' className='p-4 bg-login-50/5 rounded-md flex flex-col justify-center w-42.5'>
-                <h1 className='flex items-center gap-2 text-sm text-muted-foreground capitalize'>
+            <Link href='/sites' className='p-4 bg-login-50/5 rounded-md w-89'>
+                <h1 className='flex items-center gap-2 text-sm text-muted-foreground capitalize pb-4'>
                     <Server className='w-5' /> System
                 </h1>
-                <div className='font-bold'>
-                    <h1>{information.system.disk}</h1>
-                    <h1>{information.system.load}</h1>
-                    <h1>{information.system.processes}</h1>
-                    <h1>{information.system.ram}</h1>
-                    <h1>{information.system.containers}</h1>
+                <div className='grid grid-cols-2 gap-4'>
+                    <div>
+                        <h1 className={primary}>RAM</h1>
+                        <h1 className={secondary}>{information.system.ram}</h1>
+                        <h1 className={primary}>Processes</h1>
+                        <h1 className={secondary}>{information.system.processes}</h1>
+                        <h1 className={primary}>Disk</h1>
+                        <h1 className={secondary}>{information.system.disk}</h1>
+                    </div>
+                    <div>
+                        <h1 className={primary}>Load</h1>
+                        <h1 className={secondary}>{information.system.load}</h1>
+                        <h1 className={primary}>Containers</h1>
+                        <h1 className={secondary}>{information.system.containers}</h1>
+                    </div>
                 </div>
             </Link>
         </div>
